960db3a93ecaa304ab9fa993eac6784ee8150985
Javeria Saeed Abbasi <jiyasaeed333@gmail.com>
2022-08-08 17:48:00 +0500

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,50 @@
+const seconds = document.querySelector('.second-hand');
+const minutes = document.querySelector('.min-hand');
+const hours = document.querySelector('.hour-hand');
+let numb = "12";
+let drawnumber = function() {
+
+    for (let i = 1; i < 13; i++) {
+  
+      let el = document.createElement('div');
+      el.setAttribute('class', `number number${i}`);
+      el.style.transform = `rotate(${i*30}deg)`;
+  
+      document.querySelector('.numbers').appendChild(el).innerHTML = numb;
+
+      numb = i ;
+      console.log(numb)
+    //   document.getElementById("numbe").innerHTML = numb;
+    }
+  
+  }; 
+  drawnumber()
+
+function setDate () {
+    const currentDate = new Date();
+//GET SECONDS:
+const getSec = currentDate.getSeconds();
+const secDeg = ((getSec / 60) * 360) + 90;
+seconds.style.transform = `rotate(${secDeg}deg)`;
+// console.log(getSec);
+// console.log(secDeg);
+
+//GET MINUTES:
+const getMins = currentDate.getMinutes();
+const minsDeg = ((getMins / 60) * 360) + ((getSec/60)*6) + 90;
+minutes.style.transform = `rotate(${minsDeg}deg)`;
+// console.log(getMins);
+// console.log(minsDeg);
+
+//GET HOURS:
+const getHours = currentDate.getHours();
+const hourDeg = ((getHours / 12) * 360) + ((getMins/60)*30) + 90;
+hours.style.transform = `rotate(${hourDeg}deg)`;
+// console.log(getHours);
+// console.log(hourDeg);
+
+
+}
+setInterval(setDate, 1000);
+
+setDate();
